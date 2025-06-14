1a0f5cd789f83eaa5085339d4250c248b7938f72
steve kieu <msh.computing@gmail.com>
2025-06-14 10:32:46 +1000

    Add blockinfile mode for lineinfile command

--- plays/lineinfile/main.go
+++ plays/lineinfile/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"os"
@@ -13,15 +14,24 @@ import (
 
 func main() {
 	optFlag := pflag.NewFlagSet("opt", pflag.ExitOnError)
-	inserfater := optFlag.StringP("insertafter", "a", "", "insertafter")
-	insertbefore := optFlag.StringP("insertbefore", "b", "", "insertbefore")
-	line := optFlag.StringP("line", "l", "", "Line to insert")
+	inserfater := optFlag.StringP("insertafter", "a", "", "insertafter. In blockinfile it is a json list of regex string used as upperBound")
+	insertbefore := optFlag.StringP("insertbefore", "b", "", "insertbefore. In blockinfile it is a json list of regex string used as lowerBound")
+	line := optFlag.StringP("line", "l", "", "Line(s) to insert. Can contains regex capture if your regex option has it - like $1, $2 etc..")
 	// filename := pflag.StringP("file", "f", "", "Filename or path")
-	regexptn := optFlag.StringP("regexp", "r", "", "regexp")
-	search_string := optFlag.StringP("search_string", "s", "", "search string")
+	regexptn := optFlag.StringP("regexp", "r", "", "regexp to match for mode regex_search, Can contains group capture. In blockinfile mode it is a json list of regex string used as the marker")
+	search_string := optFlag.StringP("search_string", "s", "", "search string. This is used in non regex mode")
 	backup := optFlag.Bool("backup", true, "backup")
-	cmd_mode := optFlag.StringP("cmd", "c", "lineinfile", "Command; choice lineinfile, search_replace")
-	state := optFlag.String("state", "present", "state; choices: present, absent, print. Print only print lines of matches but do nothing")
+	cmd_mode := optFlag.StringP("cmd", "c", "lineinfile", `Command; choices:
+lineinfile - insert or make sure the line exist matching the search_string if set or insert new one
+search_replace - insert or make sure the line exist matching the regex pattern
+blockinfile - make sure the block lines exists in file`)
+	state := optFlag.String("state", "present", `state; choices:
+present         - line present.
+absent          - remove line
+includeboundary - same as present used in blockinfile; the block itself contain the upper and lower boundary.
+In blockinfile mode this is the default. If you do not want set state=keepboundary
+keepboundary    - See above
+print           - Print only print lines of matches but do nothing`)
 	grep := optFlag.StringP("grep", "g", "", "Simulate grep cmd. It will set state to print and take -r for pattern to grep")
 	filename_ptn := optFlag.StringP("fptn", "f", ".*", "Filename regex pattern")
 	exclude := optFlag.StringP("exclude", "e", "", "Exclude file name pattern")
@@ -29,6 +39,9 @@ func main() {
 	skipBinary := optFlag.BoolP("skipbinary", "y", false, "Skip binary file")
 	debug := optFlag.Bool("debug", false, "Enable debugging")
 
+	if len(os.Args) < 2 {
+		panic("missing file argument. Run with option -h for help")
+	}
 	file_path := os.Args[1]
 	optFlag.Usage = func() {
 		fmt.Printf("Usage: %s [filename/path] [opt]\n", os.Args[0])
@@ -97,6 +110,18 @@ func main() {
 				}
 				count := u.SearchReplaceFile(path, *regexptn, *line, -1, *backup)
 				output[path] = append(output[path], []interface{}{nil, count})
+			case "blockinfile":
+				// In this mode we will take these option - insertafter, insertbefore and regexp as upperBound, lowerBound and marker to call the function. They should be a json list of regex string if defined or empty
+				if *state == "present" || *state != "keepboundary" {
+					*state = "includeboundary"
+				}
+				upperBound, lowerBound, marker := []string{}, []string{}, []string{}
+				u.CheckErr(json.Unmarshal([]byte(*inserfater), &upperBound), "Unmarshal upperBound")
+				u.CheckErr(json.Unmarshal([]byte(*insertbefore), &lowerBound), "Unmarshal lowerBound")
+				u.CheckErr(json.Unmarshal([]byte(*regexptn), &marker), "Unmarshal marker")
+
+				u.BlockInFile(path, upperBound, lowerBound, marker, *line, *state == "keepboundary", false)
+
 			default:
 				panic("Unknown command " + *cmd_mode)
 			}
